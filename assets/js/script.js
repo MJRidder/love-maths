14ec9c683f47c6b07aa0c2087bedeb15ebb531fa
MJ Ridder <maarten_ridder@hotmail.com>
2024-10-02 10:22:21 +0000

    added the multiply functionalities

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,10 +30,19 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}.Aborting!`;
     }
+
+    if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+
+    }    
 }
 /**
  * Checks the answer against the first element in
@@ -67,8 +76,10 @@ function calculateCorrectAnswer() {
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById('operator').innerText;
 
-    if (operator = "+") {
+    if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`)
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -102,7 +113,11 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 // added below additional question - not part of the original videos but something to complete myself
