0e934e5b9bbf15283fe8c3b9843b3226e7bbb940
MJ Ridder <maarten_ridder@hotmail.com>
2024-10-02 08:32:36 +0000

    added math attributes for the function rungame

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gametype = this.getAttribute("data-type");
+                alert(`You clicked ${gametype}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -29,7 +47,7 @@ function displaySubtractQuestion() {
 function displayMultiplyQuestion() {
 
 }
-
+// added below additional question - not part of the original videos but something to complete myself
 function displayDivisionQuestion() {
 
 }
